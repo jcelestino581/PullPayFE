f987a2bdb41dda141e375d36c8114afc09fb78aa
Jesus <jcelestino581@gmail.com>
2024-12-15 20:43:33 -0800

    Update theme-provider.tsx

--- pullpayfe/src/components/theme-provider.tsx
+++ pullpayfe/src/components/theme-provider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-<<<<<<< Updated upstream
 
 export function ThemeProvider({
   children,
@@ -10,6 +9,4 @@ export function ThemeProvider({
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-=======
 
->>>>>>> Stashed changes
